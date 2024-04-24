2a9bd46460c3c6d46e1ce1d8f8ad0f31686ad97b
Bruno-William-0 <bruwill04@gmail.com>
2024-04-23 22:26:34 -0300

    Rotas

--- src/routes/moduloDescricao/moduloDescricao.routes.ts
+++ src/routes/moduloDescricao/moduloDescricao.routes.ts
@@ -3,9 +3,10 @@ import ModuloDescricaoController from '../../controllers/moduloDescricao/moduloD
 
 const moduloDescricaoRoutes = Router()
 
-moduloDescricaoRoutes.post('/novadescricao', ModuloDescricaoController.createModuloDescricao)
-moduloDescricaoRoutes.get('/listadescricao', ModuloDescricaoController.getModuloDescricao)
-moduloDescricaoRoutes.delete('/:id', ModuloDescricaoController.destroyModuloDescricao)
-moduloDescricaoRoutes.put('/:id', ModuloDescricaoController.updateModuloDescricao)
+moduloDescricaoRoutes.post('/save', ModuloDescricaoController.createModuloDescricao)//cria
+moduloDescricaoRoutes.get('/get', ModuloDescricaoController.getModuloDescricao)//lista
+moduloDescricaoRoutes.get('/get/:id', ModuloDescricaoController.getModuloDescricaoById)//lista por id
+moduloDescricaoRoutes.delete('/delete/:id', ModuloDescricaoController.destroyModuloDescricao)//deleta, destroi, extermina
+moduloDescricaoRoutes.put('/update/:id', ModuloDescricaoController.updateModuloDescricao)//atualiza
 
 export default moduloDescricaoRoutes
