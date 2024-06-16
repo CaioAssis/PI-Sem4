b9b598196a1eba31fe9c80f60f2e6be931b273ce
Caio Assis <caio.ab.assis@gmail.com>
2024-06-16 17:40:25 -0300

    adiciona cliente em maquina

--- src/models/vistoria.entity.ts
+++ src/models/vistoria.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm"
 import Maquina from "./maquina.entity"
+import Funcionario from "./funcionario.entity"
 @Entity()
 export default class Vistoria extends BaseEntity {
     @PrimaryGeneratedColumn()
@@ -19,5 +20,8 @@ export default class Vistoria extends BaseEntity {
 
     @ManyToOne(() => Maquina, maquina => maquina.vistorias)
     maquina!: Maquina
+
+    @ManyToOne(() => Funcionario, funcionario => funcionario.vistoria)
+    funcionario!: Funcionario
 
 }
